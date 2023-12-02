9d12ec3ea429a068880c92e3b30ce0f13fa8b62e
DamasoMagno <limamdamaso@gmail.com>
2023-12-02 19:09:07 -0300

    style: background tag replaced of zin to zinc

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.li`
   padding: 0.5rem 1rem;
   border-radius: 8px;
   color: ${(props) => props.theme.colors['--gray-300']};
-  background-color: ${(props) => props.theme.colors['--zin-850']};
+  background-color: ${(props) => props.theme.colors['--zinc-850']};
   font-size: 0.875rem;
   list-style: none;
   display: flex;
